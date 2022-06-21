1098676ee4d2d1bc824a9b53ee6bf95d26f2bea5
Andy Liggett <droobilicious@gmail.com>
2022-06-21 20:48:36 +0100

    poc

---
+++ script/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+
+    let width=20;
+    let height=20;
+    let blockCount = width * height;
+    let onColour = "rgb(0, 0, 0)";
+    let offColour = "rgb(255, 255, 255)";
+
+    $( '#biggrid' ).css( "grid-template-columns", "repeat(" + width + ", 1fr)" );
+
+    $( this ).css( "grid-template-columns", "repeat(2, 400px)" );
+
+
+    for (let i = 0;i<blockCount;i++){
+        $( '<div>' )
+            .addClass('block')
+            .appendTo( '#biggrid')
+            .css('background-color', offColour)
+            .click(function(){
+                let newColour = $(this).css( 'background-color' ) == onColour ? offColour : onColour;
+                $(this).css( 'background-color', newColour) ;
+            });
+    }
+
+
+});
